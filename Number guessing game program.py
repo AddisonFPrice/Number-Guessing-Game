48368642ae553bd2d94a081ad15d84fa474a7b73
AddisonFPrice <addison.price@juiceanalytics.com>
2018-11-29 20:57:45 -0500

    Dev Notes

--- Number guessing game program.py
+++ Number guessing game program.py
@@ -1,12 +1,16 @@
 ##Number guessing game
 
+
+#Here, I'm creating the logic that should be returned as "true" if the user guesses the right number.
 def magic_number_logic(num1, num2):
 	return num1 == num2
 
+#Setting the magic number that the user is trying to guess. The above method will only return "true"  if this number is input buy the user.
 def magic_number():
 	number = 3
 	return number
 
+#This puts it all together. There is a user prompt and input and then an  If, else statement that will print a message back to them. 
 def magic_number_game():
 	user_guess: int(input("Pick a number between 0 and 10 "))
 	if magic_number_logic(user_guess, magic_number):
@@ -14,3 +18,6 @@ def magic_number_game():
 	else:
 		 print ("Guess Again!")
 
+
+#Notes for future dev plans:
+#consider adding logic to let the user know to guess higher or lower, depending on their response. 
